59d9435cfd317c54792a9bc0f48f030a3f0e5a62
KevinGitari <Kevin>
2024-09-08 20:54:41 +0300

    lib/dog.py

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
-class Person:
-    # Class body goes here
+# Class body goes here
 
-    #Instance method definition
-    pass
+class Person:
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.") 
+
+ #Instance method definition 
+
+Perso= Person()
+Perso.talk()
+
+walk = Person()
+walk.walk()
+
+    
